d093df00d4960fba68e89e8f5bb0184c287653f6
Emilia-Dumitru <dumitruemilia41@gmail.com>
2024-06-16 13:24:41 +0300

    ex1 finished

--- ex-OOP.js
+++ ex-OOP.js
@@ -7,23 +7,18 @@
 
 //Declaram clasa si un constructor care le inițializează folosind this.
 class Car {
-    #brand;
-    #km;
-    #model;
-    #color;
-
     constructor(brand, model, color, km) {
-        this.#brand = brand;
-        this.#model = model;
-        this.#color = color;
-        this.#km = km;
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.km = km;
     }
     // Definește o metodă de tip `getter` numita `proprietati` care va returna proprietățile mașinii 
     get properties() {
-        return `This is the car I want to buy: ${this.#brand}, ${this.#model}, ${this.#color}, ${this.#km}`;
+        return `This is the car I want to buy: ${this.brand}, ${this.model}, ${this.color}, ${this.km} km`;
     }
 }
-let myFavCar = new Car('Volvo', 'XC-60', 'Negru', 234000);
+let myFavCar = new Car('Volvo', 'XC-60', 'Black', 234000);
 console.log(myFavCar.properties);
 
 /*La final, definește separat o funcție numită main, in care se vor crea 3 instanțe ale clasei mașina folosind operatorul new. 
@@ -31,3 +26,13 @@ Fiecare instanță va fi memorata Într-o variabilă separată și va avea propr
 Afișează proprietățile mașinilor instanțiate folosind getter-ul proprietati.
 */
 
+function main() {
+    const car1 = myFavCar;
+    const car2 = new Car('Mazda', 'CX-5', 'Green', 111000);
+    const car3 = new Car('Dacia', 'BIGGSTER', 'Blue', 20000)
+
+    console.log(car1.properties);
+    console.log(car2.properties);
+    console.log(car3.properties);
+}
+main();
